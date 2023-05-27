d9401af12d76d7da096ecbe5758515ed4cb9ffc6
grantwhitney1 <grant.whitney@selu.edu>
2023-05-26 22:19:02 -0500

    ingredients crud

--- Controllers/Api/Ingredients/IngredientsController.cs
+++ Controllers/Api/Ingredients/IngredientsController.cs
@@ -58,9 +58,84 @@ namespace Spice_Wise.Controllers.Api.Ingredients
         [HttpGet]
         public async Task<ActionResult> GetAll()
         {
-            var models = await _context.Ingredients.ToListAsync();
+            var models = await _context
+                .Ingredients
+                .ToListAsync();
 
             return Ok(models);
+        }
+
+        [HttpGet("{Id:int}")]
+        public async Task<ActionResult> Get(int Id)
+        {
+            var model = await _context
+                .Ingredients
+                .FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (model == null)
+                return NotFound("Could not find an ingredient with that Id in the database.");
+
+            return Ok(model);
+        }
+
+        [HttpPut("{Id:int}")]
+        public async Task<ActionResult> Put(int Id, IngredientPostDto dto)
+        {
+            var model = await _context
+                .Ingredients
+                .FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (model == null)
+                return NotFound("Could not find an ingredient with that Id in the database.");
+
+            //Validation
+
+            if (dto == null)
+                return BadRequest("Cannot post a null entity.");
+
+            if (dto.Name.IsNullOrEmpty() || dto.Unit.IsNullOrEmpty())
+                return BadRequest("This field cannot be empty.");
+
+            if (dto.Name.Length > 24)
+                return BadRequest("This field cannot exceed 24 characters.");
+
+            if (dto.Name.Length < 3)
+                return BadRequest("This field cannot be less than 3 characters.");
+
+            if (!await _context.IngredientCategories.AnyAsync(x => x.Id == dto.IngredientCategoryId))
+                return NotFound("The given ingredient category cannot be found.");
+
+            //Object mapping
+
+            model.Name = dto.Name;
+            model.Unit = dto.Unit;
+            model.IngredientCategoryId = dto.IngredientCategoryId;
+            model.IngredientCategory = await _context
+                .IngredientCategories
+                .FirstOrDefaultAsync(x =>
+                    x.Id == dto.IngredientCategoryId);
+
+            //Save to DB
+
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("{Id:int}")]
+        public async Task<ActionResult> Delete(int Id)
+        {
+            var model = await _context
+                .Ingredients
+                .FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (model == null)
+                return NotFound("Could not find an ingredient with that Id in the database.");
+
+            _context.Remove(model);
+
+            return Ok();
         }
     }
 }
